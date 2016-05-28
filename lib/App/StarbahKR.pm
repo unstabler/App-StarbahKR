6b0336f4423345b4032fe47bf092dd8f4bfcee94
unstabler <doping.cheese@gmail.com>
2016-05-28 14:58:53 +0900

    initial commit

---
+++ lib/App/StarbahKR.pm
@@ -0,0 +1,32 @@
+package App::StarbahKR;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+App::StarbahKR -
+
+=head1 SYNOPSIS
+
+  use App::StarbahKR;
+
+=head1 DESCRIPTION
+
+App::StarbahKR is
+
+=head1 AUTHOR
+
+unstabler E<lt>dopping.cheese {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
